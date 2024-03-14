9aa6d28e41e354eae881d6de11afe25a5531e439
Bogdan Zotov <zotov.bogdan2017@yandex.ru>
2024-03-14 20:18:42 +0300

    Init

--- src/Pages/Lore/LoreOne/BookLoreOne/BookLoreOne.tsx
+++ src/Pages/Lore/LoreOne/BookLoreOne/BookLoreOne.tsx
@@ -1,15 +1,14 @@
-// import styles from "./BookLoreOne.module.css"
-
+import "./BookLoreOne.css"
 import BookPageLeft from "./BookPages/BookPageLeft"
 import BookPageRight from "./BookPages/BookPageRight"
 
 const BookLoreOne: React.FC = () => {
-    return (
-        <div>
-            <BookPageLeft />
-            <BookPageRight />
-        </div> 
-    )
+  return (
+      <div className="BookStyle">
+        <div className="PageLeft"><BookPageLeft /></div>
+        <div className="PageLeft"><BookPageRight /></div>
+      </div>
+  )
 }
 
 export default BookLoreOne
--- src/Pages/Lore/LoreOne/LoreOne.tsx
+++ src/Pages/Lore/LoreOne/LoreOne.tsx
@@ -1,31 +1,37 @@
 import { useAppDispatch, useAppSelector } from "../../../app/hooks"
 import BookLoreOne from "./BookLoreOne/BookLoreOne"
 import { BackgroundShowChange } from "./Store/slices"
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 import "./LoreOne.css"
 
 const LoreOne: React.FC = () => {
-    const [buttonText, setButtonText] = useState<string>("Show"); // Используем useState для кнопки
+  const [buttonText, setButtonText] = useState<string>("Show") // Используем useState для кнопки
 
-    const state = useAppSelector(state => state.LoreOne)
-    const dispatch = useAppDispatch()
+  const state = useAppSelector(state => state.LoreOne)
+  const dispatch = useAppDispatch()
 
-    useEffect(() => {
+  useEffect(() => {
+    setButtonText(state.isShowBook ? "Show" : "Hide")
+  }, [state.isShowBook])
 
-        setButtonText(state.isShowBook ? "Show" : "Hide");
-    }, [state.isShowBook]);
+  const handleBackChange = () => {
+    dispatch(BackgroundShowChange())
+  }
 
-    const handleBackChange = () => {
-        dispatch(BackgroundShowChange());
-    }
-
-    return ( 
-        <body className="LoreOneBody">
-            <img className="BackImg" src={state.curBackground}></img>
-            <div className={state.isShowBook ? "Book" : "HiddenBook"}><BookLoreOne /></div>
-            <button onClick={handleBackChange} className="BackButton">{buttonText}</button>
-        </body>
-    )
+  return (
+    <body className="LoreOneBody">
+      <div className={state.isShowBook? "effects" : "noEffects"}></div>
+      <img className="BackImg" src="https://i.pinimg.com/originals/f7/83/41/f7834110336ed7fb68f8f0d7327b34cc.jpg"></img>
+      <div className={state.isShowBook ? "Book" : "HiddenBook"}>
+        <BookLoreOne />
+      </div>
+      <button onClick={handleBackChange} className="BackButton rounded-button">
+        <span className="left-semicircle"></span>
+        <span className="button-content">{buttonText}</span>
+        <span className="right-semicircle"></span>
+      </button>
+    </body>
+  )
 }
 
 export default LoreOne
